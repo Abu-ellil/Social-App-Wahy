020ba85efc9d0fb90ec7765a5eb03106b0da1af3
Abo Ellil <mr.abuellil@gmail.com>
2024-02-13 18:55:27 +0200

    css

--- front/src/components/Post.jsx
+++ front/src/components/Post.jsx
@@ -19,13 +19,11 @@ const Post = forwardRef(({ post, onLike, onComment, onDeleteComment }, ref) => {
   }, [post]);
 
   useEffect(() => {
-    // Save like status to local storage whenever it changes
     localStorage.setItem(`post_${post._id}_like`, isLiked);
   }, [post._id, isLiked]);
 
   const handleLike = async () => {
     setIsLiked((prevIsLiked) => !prevIsLiked);
-    // Call onLike function to handle the like action
     onLike(post._id, isLiked);
   };
 
@@ -35,10 +33,9 @@ const Post = forwardRef(({ post, onLike, onComment, onDeleteComment }, ref) => {
   };
 
   const handleDeleteComment = async (commentId) => {
-    // Call onDeleteComment function to handle the comment deletion
     onDeleteComment(post._id, commentId);
   };
-
+console.log(post.user._id)
   return (
     <div ref={ref} className="post">
       {user ? (
@@ -53,29 +50,32 @@ const Post = forwardRef(({ post, onLike, onComment, onDeleteComment }, ref) => {
             {post.image && <img src={post.image} alt="Post" />}
             <div className="actions">
               <p>{post.likes.length}</p>
-              <button onClick={handleLike}>
-                {isLiked ? "Unlike" : "Like"}
-              </button>
-              <button onClick={handleComment}>Comment</button>
             </div>
           </div>
           {/* Render comments */}
           <div className="comments">
-            {post.comments.map((comment) => (
-              <div key={comment._id} className="comment">
-                {comment.user && comment.user.profilePhoto && (
-                  <img
-                    src={comment.user.profilePhoto.url}
-                    alt="Comment User Profile"
-                  />
-                )}
-                <p>{comment.text}</p>
-                {/* Delete button for each comment */}
-                <button onClick={() => handleDeleteComment(comment._id)}>
-                  Delete
-                </button>
-              </div>
-            ))}
+            <button onClick={handleLike}>{isLiked ? "Unlike" : "Like"}</button>
+
+            <div>
+              {post.comments.map((comment) => (
+                <div key={comment._id} className="comment">
+                  {console.log(comment.user.id)}
+                  {comment.user && comment.user.profilePhoto && (
+                    <img
+                      src={comment.user.profilePhoto.url}
+                      alt="Comment User Profile"
+                    />
+                  )}
+                  <p>{comment.text}</p>
+                  {/* Delete button for each comment */}
+                  {comment.user.id === user._id && (
+                    <button onClick={() => handleDeleteComment(comment._id)}>
+                      Delete
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </>
       ) : (
