9e9c97dd9928c749e5fe3c5ae6a103e7aa2a30c3
Abo Ellil <mr.abuellil@gmail.com>
2024-02-14 06:30:26 +0200

    ناف بار

--- front/src/components/Nav.jsx
+++ front/src/components/Nav.jsx
@@ -7,40 +7,19 @@ const Navbar = () => {
   const user = useSelector((state) => state.user);
 
   return (
-    <nav className="navbar">
-      <div className="user-status">
-        <img
-          src={user && user.profilePhoto && user.profilePhoto.url}
-          alt="User"
-          className="user-image"
-        />
-        <h6>{user && user.username && user.username}</h6>
-      </div>
-      <div className="user-status">
-        <img
-          src={user && user.profilePhoto && user.profilePhoto.url}
-          alt="User"
-          className="user-image"
-        />
-        <h6>{user && user.username && user.username}</h6>
-      </div>
-      <div className="user-status">
-        <img
-          src={user && user.profilePhoto && user.profilePhoto.url}
-          alt="User"
-          className="user-image"
-        />
-        <h6>{user && user.username && user.username}</h6>
-      </div>
-      <div className="user-status">
-        <img
-          src={user && user.profilePhoto && user.profilePhoto.url}
-          alt="User"
-          className="user-image"
-        />
-        <h6>{user && user.username && user.username}</h6>
-      </div>
-    </nav>
+    user && (
+      <nav className="navbar">
+        <div className="user-status">
+          <img
+            src={user.profilePhoto && user.profilePhoto.url}
+            alt="User"
+            className="user-image"
+          />
+          <h6>{user.username && user.username}</h6>
+        </div>
+       
+      </nav>
+    )
   );
 };
 
