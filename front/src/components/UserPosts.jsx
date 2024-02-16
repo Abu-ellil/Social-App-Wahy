25b94e39e73e46e963ffad1e9ba872c8c4df5344
Abo Ellil <mr.abuellil@gmail.com>
2024-02-16 06:22:42 +0200

    ....

--- front/src/components/UserPosts.jsx
+++ front/src/components/UserPosts.jsx
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from "react-toastify";
 
 const UserPosts = ({ userId }) => {
   const user = useSelector((state) => state.user);
-  const apiUrl = "https://wahy-social-app-api.onrender.com/";
+  const apiUrl = "https://wahy-social-app-api.onrender.com";
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [editPostId, setEditPostId] = useState(null);
