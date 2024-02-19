2e059e7bca341126e5fb4960e0f69aea1363e1d3
Abo Ellil <mr.abuellil@gmail.com>
2024-02-19 21:13:26 +0200

    Link

--- front/src/components/sideBar/SideMenu.jsx
+++ front/src/components/sideBar/SideMenu.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-icons/fa";
 import { RiLoginCircleLine, RiLogoutCircleLine } from "react-icons/ri";
 import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTheme } from "../../context/ThemeContext";
 import { useGetUserToken } from "../../hooks/useGetUserToken";
 import LanguageToggle from "../../languages/LanguageToggle";
@@ -30,46 +30,46 @@ const SideMenu = () => {
 
   return (
     <div className="side-menu">
-      <NavLink to="/" className="navbar-link">
+      <Link to="/" className="navbar-link">
         <FaHome className="ico" />
         <h2>{t("home")}</h2>
-      </NavLink>
+      </Link>
 
       {token && (
-        <NavLink to="/search" className="navbar-link">
+        <Link to="/search" className="navbar-link">
           <FaSearch className="ico" />
           <h2>{t("search")}</h2>
-        </NavLink>
+        </Link>
       )}
 
       {token && (
-        <NavLink to="/" className="navbar-link">
+        <Link to="/" className="navbar-link">
           <FaWpexplorer className="ico" />
           <h2>{t("explore")}</h2>
-        </NavLink>
+        </Link>
       )}
 
       {token && (
-        <NavLink to="/post-form" className="navbar-link">
+        <Link to="/post-form" className="navbar-link">
           <FaPlusCircle className="ico" />
           <h2>{t("create")}</h2>
-        </NavLink>
+        </Link>
       )}
 
       {user ? (
         <>
-          <NavLink to="/edit-profile" className="navbar-link">
+          <Link to="/edit-profile" className="navbar-link">
             <FaRegUser className="ico" />
             <h2>{t("profile")}</h2>
-          </NavLink>
-          <NavLink to="/my-posts" className="navbar-link">
+          </Link>
+          <Link to="/my-posts" className="navbar-link">
             <img
               src={user && user.profilePhoto && user.profilePhoto.url}
               alt="User"
               className="user-image"
             />
             <h3>{user.username}</h3>
-          </NavLink>
+          </Link>
           <button onClick={handleLogout} className="logout-btn btn">
             <RiLogoutCircleLine className="ico" />
             <h2 className="log-text">{t("logout")}</h2>
@@ -77,13 +77,13 @@ const SideMenu = () => {
         </>
       ) : (
         <>
-          <NavLink to="/register" className="navbar-link">
+          <Link to="/register" className="navbar-link">
             <h2 className="log-text">{t("register")}</h2>
-          </NavLink>
-          <NavLink to="/login" className="navbar-link">
+          </Link>
+          <Link to="/login" className="navbar-link">
             <RiLoginCircleLine className="ico login-icon" />
             <h2 className="log-text">{t("login")}</h2>
-          </NavLink>
+          </Link>
         </>
       )}
       <div className="theme-lang-container">
