a5ab8ca5e94a107bcbfa5e66d1b7882f6803beb8
Abo Ellil <mr.abuellil@gmail.com>
2024-02-23 04:50:28 +0200

    Update ChatBox.jsx

--- front/src/components/chat/ChatBox.jsx
+++ front/src/components/chat/ChatBox.jsx
@@ -32,12 +32,18 @@ const ChatBox = () => {
       <Stack gap={3} className="messages">
         {messages &&
           messages.map((message, index) => (
-            <Stack key={index}>
+            <Stack
+              key={index}
+              className={`${
+                message.senderId === user?._id
+                  ? "message self align-self-end flex-grow-0"
+                  : "message align-self-start flex-grow-0"
+              }`}
+            >
               <span>{message.text}</span>
-              <span>
+              <span className="message-footer">
                 {moment(message.createdAt).format("YYYY-MM-DD HH:mm:ss")}
-              </span>{" "}
-              {/* Convert moment object to string */}
+              </span>
             </Stack>
           ))}
       </Stack>
