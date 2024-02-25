d9e89d4e5afebf92663ed38dc85753c124f63382
Abo Ellil <mr.abuellil@gmail.com>
2024-02-25 07:05:36 +0200

    #root colors css

--- front/src/components/sideBar/SideMenu.jsx
+++ front/src/components/sideBar/SideMenu.jsx
@@ -1,17 +1,15 @@
+import React, { useState, useEffect } from "react";
 import {
   FaHome,
-  FaMoon,
+  FaSearch,
+  FaWpexplorer,
   FaPlusCircle,
   FaRegUser,
-  FaSearch,
-  FaSun,
-  FaWpexplorer,
 } from "react-icons/fa";
 import { RiLoginCircleLine, RiLogoutCircleLine } from "react-icons/ri";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useTheme } from "../../context/ThemeContext";
-import logo from "../../assets/logo.png"
 import LanguageToggle from "../../languages/LanguageToggle";
 import "./sideMenu.css";
 import { useTranslation } from "react-i18next";
@@ -23,46 +21,91 @@ const SideMenu = () => {
   const user = useSelector((state) => state.user);
   const token = useGetUserToken();
 
+  const [menuVisible, setMenuVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const isVisible = prevScrollPos > currentScrollPos;
+
+    setMenuVisible(isVisible);
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     window.location.reload();
   };
 
+  const [activeLink, setActiveLink] = useState("");
+
   return (
-    <div className="side-menu">
+    <div className={`side-menu ${menuVisible ? "show" : "hide"}`}>
       <div className="logo">
       </div>
 
       <div className="side-menu-links">
-        <Link to="/" className="navbar-link">
+        <Link
+          to="/"
+          className={`navbar-link ${activeLink === "home" ? "active" : ""}`}
+          onClick={() => setActiveLink("home")}
+        >
           <FaHome className="ico" />
           <h2>{t("home")}</h2>
         </Link>
 
         {token && (
-          <Link to="/search" className="navbar-link">
+          <Link
+            to="/search"
+            className={`navbar-link ${activeLink === "search" ? "active" : ""}`}
+            onClick={() => setActiveLink("search")}
+          >
             <FaSearch className="ico" />
             <h2>{t("search")}</h2>
           </Link>
         )}
 
         {token && (
-          <Link to="/" className="navbar-link">
+          <Link
+            to="/"
+            className={`navbar-link ${
+              activeLink === "explore" ? "active" : ""
+            }`}
+            onClick={() => setActiveLink("explore")}
+          >
             <FaWpexplorer className="ico" />
             <h2>{t("explore")}</h2>
           </Link>
         )}
 
         {token && (
-          <Link to="/post-form" className="navbar-link">
+          <Link
+            to="/post-form"
+            className={`navbar-link ${activeLink === "create" ? "active" : ""}`}
+            onClick={() => setActiveLink("create")}
+          >
             <FaPlusCircle className="ico" />
             <h2>{t("create")}</h2>
           </Link>
         )}
 
         {token && (
-          <Link to="/edit-profile" className="navbar-link">
+          <Link
+            to="/edit-profile"
+            className={`navbar-link ${
+              activeLink === "profile" ? "active" : ""
+            }`}
+            onClick={() => setActiveLink("profile")}
+          >
             <FaRegUser className="ico" />
             <h2>{t("profile")}</h2>
           </Link>
@@ -70,19 +113,15 @@ const SideMenu = () => {
       </div>
 
       {user ? (
-        <>
-          <button onClick={handleLogout} className="logout-btn">
-            <RiLogoutCircleLine className="ico" />
-            <h2 className="log-text">{t("logout")}</h2>
-          </button>
-        </>
+        <button onClick={handleLogout} className="logout-btn">
+          <RiLogoutCircleLine className="ico" />
+          <h2 className="log-text">{t("logout")}</h2>
+        </button>
       ) : (
-        <>
-          <Link to="/login" className="navbar-link">
-            <RiLoginCircleLine className="ico login-icon" />
-            <h2 className="log-text">{t("login")}</h2>
-          </Link>
-        </>
+        <Link to="/login" className="navbar-link">
+          <RiLoginCircleLine className="ico login-icon" />
+          <h2 className="log-text">{t("login")}</h2>
+        </Link>
       )}
     </div>
   );
